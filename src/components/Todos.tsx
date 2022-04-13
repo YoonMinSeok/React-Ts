4aa448bf945ba04221e2179b31c560cddac80a87
윤민석 <ykmvm147@naver.com>
2022-04-13 22:08:39 +0900

    Create todo components && Practice props and type

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,13 @@
-function Todos() {
+import React from "react";
+import Todo from "../models/Todo";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
     </ul>
   );
-}
+};
 export default Todos;
